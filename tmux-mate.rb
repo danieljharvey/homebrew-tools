18f154028f2f85271bf7a0622bce49cddf686ec0
Daniel Harvey <danieljamesharvey@gmail.com>
2020-06-06 10:41:07 +0100

    Bump tmux-mate to 0.0.2

--- tmux-mate.rb
+++ tmux-mate.rb
@@ -1,8 +1,8 @@
 class TmuxMate < Formula
   desc "Arranging squares with programs running in them made easy"
   homepage "https://github.com/danieljharvey/tmux-mate"
-  url "https://github.com/danieljharvey/tmux-mate/releases/download/v0.0.1/tmux-mate-v0.0.1-osx.tar.gz"
-  sha256 "c1eb8919650d78a81f8f5b0a736e4b5e988ec700e7d6b87058f1803c3301a629"
+  url "https://github.com/danieljharvey/tmux-mate/releases/download/v0.0.2/tmux-mate-v0.0.2-osx.tar.gz"
+  sha256 "f93d5c3527c1cdb1a7217b18f8bba6318cbecf361cfea51aa5163904f6bc3a66"
 
   bottle :unneeded
 
