8b0dc2997504550aacc11f635138a1a887b7837f
Daniel Harvey <danieljamesharvey@gmail.com>
2020-06-12 07:53:48 +0100

    Oops

--- nix-mate.rb
+++ nix-mate.rb
@@ -2,7 +2,7 @@ class NixMate < Formula
   desc "Nix Shells made easy"
   homepage "https://github.com/danieljharvey/nix-mate"
   url "https://github.com/danieljharvey/nix-mate/releases/download/v0.0.4/nix-mate-v0.0.4-osx.tar.gz"
-  sha256 "b6ec32f5f47e3d17da228465a51e3a758096e5768695a70e7ff94e809d0c137f"
+  sha256 "62203218fa3c989094e1a59f579647d265d7168f8d1ca48ac24dd3ee58cb3a46"
 
   bottle :unneeded
 
