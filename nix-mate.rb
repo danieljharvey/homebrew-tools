1dbbdb3eda1366c2057e1cea571c0601a6af52b7
Daniel Harvey <danieljamesharvey@gmail.com>
2020-12-27 09:56:10 +0000

    Update nix-mate to 0.0.13

--- nix-mate.rb
+++ nix-mate.rb
@@ -1,13 +1,13 @@
 class NixMate < Formula
   desc "Nix Shells made easy"
   homepage "https://github.com/danieljharvey/nix-mate"
-  url "https://github.com/danieljharvey/nix-mate/releases/download/v0.0.9/nix-mate-0.0.9-osx"
-  sha256 "1d2fad7b098d2c10c9617441f30c88cfb08bc1c7553cec97dcf283aa3a5017ac"
+  url "https://github.com/danieljharvey/nix-mate/releases/download/v0.0.13/nix-mate-0.0.13-osx"
+  sha256 "d79fd161ffae0c2911628370995802595b4498771c41b2367e1c7ade430a7759"
 
   bottle :unneeded
 
   def install
-    system "mv", "nix-mate-0.0.9-osx", "nix-mate"
+    system "mv", "nix-mate-0.0.13-osx", "nix-mate"
     bin.install "nix-mate"
   end
 
