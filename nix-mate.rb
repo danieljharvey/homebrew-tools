4203303d215e139d043f02c810b6d3c4acfa6642
Daniel Harvey <danieljamesharvey@gmail.com>
2022-05-05 14:51:07 +0100

    Remove bottle:unneeded

--- nix-mate.rb
+++ nix-mate.rb
@@ -4,8 +4,6 @@ class NixMate < Formula
   url "https://github.com/danieljharvey/nix-mate/releases/download/v0.0.15/nix-mate-0.0.15-osx"
   sha256 "8fcf42b6917438c241ce42d70889649badbb4405eee36f9cb78231f51b34bffb"
 
-  bottle :unneeded
-
   def install
     system "mv", "nix-mate-0.0.15-osx", "nix-mate"
     bin.install "nix-mate"
